eff4ded3d11f8031547a4a95c24eaf079d14db80
ClownsCheese <rolands.tiss@aalto.fi>
2021-03-09 16:42:58 +0200

    marman

---
+++ Assets/Scripts/CollectableSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour
+{
+    float min;
+    float max;
+
+    public GameObject collectablePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnMarman", 3f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnMarman()
+    {
+        float y = Random.Range(-3.6f, 0f);
+        Vector3 spawnPos = new Vector3(transform.position.x, y, transform.position.z);
+        Instantiate(collectablePrefab, spawnPos, Quaternion.identity);
+    }
+}
