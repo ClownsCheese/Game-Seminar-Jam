fcebfcdbda5a119e9d744f48983b5289fb176c19
ClownsCheese <rolands.tiss@aalto.fi>
2021-03-09 16:59:54 +0200

    semi final

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text pointsTXT;
+    public int points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePoints(int pointsToAdd)
+    {
+        points += pointsToAdd;
+        pointsTXT.text = $"{points}";
+    }
+}
--- Assets/Scripts/MarmanBehaviour.cs
+++ Assets/Scripts/MarmanBehaviour.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MarmanBehaviour : MonoBehaviour
 {
     public float speed;
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -19,11 +20,15 @@ public class MarmanBehaviour : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Debug.Log("ddd");
+        gameManager.UpdatePoints(Random.Range(2, 100));
         Destroy(gameObject);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        //Debug.Log("ddd");
+
+        //Destroy(gameObject);
     }
 }
